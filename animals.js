a69b6865e3980383abb58390be03d92ba79e4b94
EnzoTM1170 <143804193+EnzoTM1170@users.noreply.github.com>
2023-10-26 22:52:14 -0300

    Delete animals.js

--- animals.js
+++
@@ -1,7 +0,0 @@
-const animals = ["peixe", "cachorro", "coelho", "anta", "candango", "leão"];
-if(!animals.includes("leão")){
-    console.log("Leão não existe no array animals.");
-}
-else{
-    console.log("Existe um leão no array animals.");
-}
